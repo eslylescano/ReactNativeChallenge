a0369fd441c39b802ae1c1da823a0b34f0e81b0c
Esly Lescano <eslylescano@gmail.com>
2025-02-04 22:18:19 +0100

    Test card component and fix jext for icons

--- components/Card/Card.test.tsx
+++ components/Card/Card.test.tsx
@@ -1,12 +1,58 @@
 import { render, screen } from "@testing-library/react-native";
 import { composeStories } from "@storybook/react";
-
 import * as stories from "./Card.stories";
-const { Default } = composeStories(stories);
 
-describe("Card", () => {
-  it("renders correctly", () => {
+const {
+  Default,
+  ModerateIntensity,
+  HighIntensity,
+  LowIntensity,
+  VeryHighIntensity,
+} = composeStories(stories);
+
+describe("Card Component", () => {
+  it("renders Default story correctly", () => {
     render(<Default />);
     expect(screen.getByTestId("CardContainer")).toBeTruthy();
+    expect(screen.getByText("12:00 - 12:30")).toBeTruthy();
+    expect(screen.getByText("266")).toBeTruthy();
+    expect(screen.getByText("263")).toBeTruthy();
+    expect(screen.getByText("Moderate")).toBeTruthy();
+  });
+
+  it("renders Moderate Intensity story correctly", () => {
+    render(<ModerateIntensity />);
+    expect(screen.getByTestId("CardContainer")).toBeTruthy();
+    expect(screen.getByText("12:00 - 12:30")).toBeTruthy();
+    expect(screen.getByText("266")).toBeTruthy();
+    expect(screen.getByText("263")).toBeTruthy();
+    expect(screen.getByText("Moderate")).toBeTruthy();
+  });
+
+  it("renders High Intensity story correctly", () => {
+    render(<HighIntensity />);
+    expect(screen.getByTestId("CardContainer")).toBeTruthy();
+    expect(screen.getByText("12:30 - 13:00")).toBeTruthy();
+    expect(screen.getByText("267")).toBeTruthy();
+    expect(screen.getByText("265")).toBeTruthy();
+    expect(screen.getByText("High")).toBeTruthy();
+  });
+
+  it("renders Low Intensity story correctly", () => {
+    render(<LowIntensity />);
+    expect(screen.getByTestId("CardContainer")).toBeTruthy();
+    expect(screen.getByText("13:00 - 13:30")).toBeTruthy();
+    expect(screen.getByText("200")).toBeTruthy();
+    expect(screen.getByText("190")).toBeTruthy();
+    expect(screen.getByText("Low")).toBeTruthy();
+  });
+
+  it("renders Very High Intensity story correctly", () => {
+    render(<VeryHighIntensity />);
+    expect(screen.getByTestId("CardContainer")).toBeTruthy();
+    expect(screen.getByText("13:30 - 14:00")).toBeTruthy();
+    expect(screen.getByText("300")).toBeTruthy();
+    expect(screen.getByText("280")).toBeTruthy();
+    expect(screen.getByText("Very High")).toBeTruthy();
   });
 });
--- jest.setup.ts
+++ jest.setup.ts
@@ -4,4 +4,9 @@ import "@testing-library/react-native/extend-expect";
 
 jest.useFakeTimers();
 
+jest.mock('expo-font', () => ({
+    loadAsync: jest.fn().mockResolvedValue(true),
+    isLoaded: jest.fn().mockReturnValue(true),
+  }));
+
 setProjectAnnotations([previewAnnotations]);
