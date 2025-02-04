e5154026c0213d75d049159e32ceb1aaf0dedddd
Esly Lescano <eslylescano@gmail.com>
2025-02-04 22:17:51 +0100

    Add more stories to test

--- components/Card/Card.stories.tsx
+++ components/Card/Card.stories.tsx
@@ -32,3 +32,59 @@ export const Default: Story = {
     },
   },
 };
+
+export const ModerateIntensity: Story = {
+  args: {
+    data: {
+      from: "2018-01-20T12:00Z",
+      to: "2018-01-20T12:30Z",
+      intensity: {
+        forecast: 266,
+        actual: 263,
+        index: "moderate",
+      },
+    },
+  },
+};
+
+export const HighIntensity: Story = {
+  args: {
+    data: {
+      from: "2018-01-20T12:30Z",
+      to: "2018-01-20T13:00Z",
+      intensity: {
+        forecast: 267,
+        actual: 265,
+        index: "high",
+      },
+    },
+  },
+};
+
+export const LowIntensity: Story = {
+  args: {
+    data: {
+      from: "2018-01-20T13:00Z",
+      to: "2018-01-20T13:30Z",
+      intensity: {
+        forecast: 200,
+        actual: 190,
+        index: "low",
+      },
+    },
+  },
+};
+
+export const VeryHighIntensity: Story = {
+  args: {
+    data: {
+      from: "2018-01-20T13:30Z",
+      to: "2018-01-20T14:00Z",
+      intensity: {
+        forecast: 300,
+        actual: 280,
+        index: "veryhigh",
+      },
+    },
+  },
+};
